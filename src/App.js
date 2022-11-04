27a452d6caf552634f7aa94284486dcd0bdbe045
Roshan Uchai <thakuriroshan863@gmail.com>
2022-11-04 11:05:06 +1100

    setupWork

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Assessment second
+         
         </p>
         <a
           className="App-link"
